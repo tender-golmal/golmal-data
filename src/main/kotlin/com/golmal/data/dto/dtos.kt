074b17fffd5debb4d08f9cb63cddfb94816ce43e
sushant patil <sushant.patil@lendico.de>
2021-01-03 16:29:55 +0100

    Login/api response object added

--- src/main/kotlin/com/golmal/data/dto/dtos.kt
+++ src/main/kotlin/com/golmal/data/dto/dtos.kt
@@ -1,5 +1,7 @@
 package com.golmal.data.dto
 
+import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
+
 data class RegisterRequest(
     val email:String,
     val password:String,
@@ -10,3 +12,7 @@ data class LoginRequest(
     val email:String,
     val password: String
 )
+data class LoginResponse(
+    val token:String
+)
+data class ApiResponse(val status:Int, val message:JvmType.Object, val result:JvmType.Object)
