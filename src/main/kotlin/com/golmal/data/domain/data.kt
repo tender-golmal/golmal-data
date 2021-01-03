6afc3de7bc9f465af5adbee3e9932cd26e18cd05
sushant patil <sushant.patil@lendico.de>
2021-01-04 00:43:28 +0100

    Document id added

--- src/main/kotlin/com/golmal/data/domain/data.kt
+++ src/main/kotlin/com/golmal/data/domain/data.kt
@@ -7,6 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document
 @Document("users")
 data class UserProfile(
     @Id
+    val id:String,
     val email:String,
     var firstName:String?,
     var lastName:String?,
